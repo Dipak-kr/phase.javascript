cd21ea976aedd232c487480665053cd6aecd8703
dipak-kr <dipakshah3321@gmail.com>
2025-07-11 21:08:15 +0545

    conversion on datatypes

--- 01_basics/03_conversionOperators.js
+++ 01_basics/03_conversionOperators.js
@@ -1,10 +1,10 @@
 let score = true;
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let ValueInNumber = Number(score);
-console.log(typeof ValueInNumber);
-console.log(ValueInNumber);
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
 
 //1=> true; 0=>false
 //"" => false
@@ -13,5 +13,20 @@ console.log(ValueInNumber);
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+//********************Operations************** */
+// let value=3
+// let negValue = -value;
+// // console.log(negValue);
+
+// let str1 = "Hello"
+// let str2 =" Dipak"
+
+// let str3 = str1+str2;
+// /
+
+let gameCounter =100;
+++gameCounter;
+console.log(gameCounter);
