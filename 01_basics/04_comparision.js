e736410afab1524fa2c09ce42f15fec1b48a6b77
dipak-kr <dipakshah3321@gmail.com>
2025-07-11 21:45:05 +0545

    comparisions on datatype

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// avoid this type of comparison this gives confusions 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
