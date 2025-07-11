9861b001ed7159eef79cb5af3db2d42114849bdc
dipak-kr <dipakshah3321@gmail.com>
2025-07-11 10:22:57 +0545

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 144553
+let accountEmail = "dipakshah3321@gmail.com"
+var accountPassword = "12332"
+accountCity = "sunasri"
+
+
+// accountId = 2
+accountEmail = "dipakshah@gmail.com"
+accountPassword = "086060"
+accountCity = "brt"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+we learn to use 
+Var
+let    *
+const  *
+
+Prefer not to use 'var'
+because of issue in block and scope.....
+
+
+console.log("for particular print");
+console.table("for group print");
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as new verson
+// alert("Dipak shah") we use only nodejs,not browser
+ 
+
+let name ="Dipak";
+let age = 22;
+let isLoggedIn = false;
+
+
+// number => 2 to power 53
+// BigInt
+// string  => ""
+// Boolean
+// null => standalone Value
+// undefined =>
+// symbol => Unique
+
+// NN SS BB U
+
+console.log(typeof name);
