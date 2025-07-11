269d31336a6a700292068fe348adb5bdcece7f65
dipak-kr <dipakshah3321@gmail.com>
2025-07-11 20:47:41 +0545

    conversion of datatypes

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,17 @@
+let score = true;
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber = Number(score);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+//1=> true; 0=>false
+//"" => false
+//"Dipak" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
