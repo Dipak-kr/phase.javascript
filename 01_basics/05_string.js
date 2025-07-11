6d257edf95211114d15f1a6c2e1153f71796985c
dipak-kr <dipakshah3321@gmail.com>
2025-07-11 22:40:51 +0545

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,27 @@
+const name="Dipak";
+const surename ="shah";
+// console.log(name + surename +"02");
+
+// console.log('My name is ${name} and my surename is ${surename});
+
+const gameName =new String ("OnlyXDipak");
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(6));
+console.log(gameName.indexOf('X'));
+console.log(gameName.split('--'));
+
+ //many functions are available in strings 
+ // i can use many more but later 
+
+ const newStringOne = "    Dipak    ";
+ console.log(newStringOne);
+ console.log(newStringOne.trimEnd());
+ console.log(newStringOne.trimStart());
+
+ const url='https://www.dipakshah.com/developer%232web';
+ console.log(url.replace('%232','-'));
+ console.log(url.includes('dipak'))
+ 
+ 
