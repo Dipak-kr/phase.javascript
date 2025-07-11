4d8910200356098ad0ffcb571f651b4cfa2ae804
dipak-kr <dipakshah3321@gmail.com>
2025-07-11 22:02:49 +0545

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,63 @@
+// #Primitive Data types
+
+// 7 types : String, Number, BigInt, Boolean, Undefined, Null, Symbol
+
+// ## string
+let name = "dipak";
+console.log(name);
+console.log(typeof name);
+
+//## number
+let age = 25;
+console.log(age);
+console.log(typeof age);
+
+
+// ## BigInt
+let bigInt = 2452325n
+console.log(bigInt);
+console.log(typeof bigInt);
+
+
+// ## Boolean
+let isActive = true;
+console.log(isActive);
+console.log(typeof isActive);
+
+
+// ## Undefined
+let notDefined;
+console.log(notDefined);
+
+// ## Null
+
+let emptyValue =null;
+console.log(emptyValue);
+console.log(typeof emptyValue);
+
+
+// ## symbol
+let uniqueSymbol = Symbol("unique");
+console.log(uniqueSymbol);
+console.log(typeof uniqueSymbol);
+
+
+// reference (Non-primitive) data types
+// 3 types: object, array, function
+
+
+// ## object
+let heros =["Dipak","Sagar","punam"];
+let myObject ={
+    name:"Dipak",
+    age: 24,
+    addrress:"Sunsari"
+
+};
+
+// ## array
+let myArray =[1,2,3,4,5,6,7,8];
+console.log(myArray);
+
+
+
