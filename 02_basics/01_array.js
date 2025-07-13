c8423f9d2f42967b92c20dc072f2435680928a3a
dipak-kr <dipakshah3321@gmail.com>
2025-07-13 18:42:50 +0545

    array update

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -21,12 +21,12 @@ console.log(myArr.indexOf("Dipak")); // 3
 
 //slice and splice
 
-console.log("A",myArr); 
+console.log("A",myArr); //
 const myn1 =myArr.slice(1,3);
-console.log(myn1);
+console.log(myn1);  // [ 42, 2.3 ]
 console.log("B",myArr);  // myArr.slice() does not modify the original array
 
 const myn2 = myArr.splice(1,3);
-console.log("C",myArr);
-console.log(myn2);
+console.log("C",myArr);  // myArr.splice() modifies the original array
+console.log(myn2);  // [ 32, 42, 2.3 ]
 
