0c984ee2c1a21fb3ac00e56691b46d236e09d07a
dipak-kr <dipakshah3321@gmail.com>
2025-07-18 19:16:53 +0545

    if statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,44 @@
+// if statement
+
+// //syntax
+
+// if(condition){
+//     //statement
+// }
+
+// //example
+
+// if (true) {
+//     // statement becomes true
+// }
+// if (false) {
+//   // statement becomes false 
+// }
+
+// const isUserLoggedIn = True
+// if (2!=3){
+//     // console.log('executed');
+    
+// }
+
+// const temprature = 41
+
+// if(temprature===41){
+//     console.log('yes it is greater');  
+// } else{
+//     console.log('No it is less than 41 '); 
+// }
+// console.log("Execute");
+
+// 
+
+const isUserLoggedIn = true
+const creditCard = true
+
+if(isUserLoggedIn && creditCard && 2==3){
+    console.log('Allow to buy course');  
+}
+if(isUserLoggedIn || creditCard){
+    console.log('user logged in');
+    
+}
