74a53503d979dfe873b856c2704718dc4d9e5ed1
dipak-kr <dipakshah3321@gmail.com>
2025-07-18 19:33:28 +0545

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,63 @@
+
+//syntax
+
+/*
+switch (key) {
+    case value:
+        
+       break;
+
+    default:
+       break;
+ }
+       */
+
+ /*
+
+ const month = 3
+
+ switch (month) {
+    case 1:
+        console.log("Jun");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+ 
+    default:
+        console.log('default case match');
+        break;
+ }
+
+ */
+//string aako bela mA sting nai search garne 
+/*
+ const month = "Dipak"
+
+ switch (month) {
+    case 1:
+        console.log("Jun");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case "Dipak":
+        console.log("Dipak");
+        break;il
+ 
+    default:
+        console.log('default case match');
+        break;
+ }
+
+ */
+
