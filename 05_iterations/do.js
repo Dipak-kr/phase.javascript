72303332f45a57e69b0a8f50fd3bec61beec2685
dipak-kr <dipakshah3321@gmail.com>
2025-07-20 19:41:42 +0545

    Do_while loops

---
+++ 05_iterations/do.js
@@ -0,0 +1,31 @@
+// do while loo
+
+let index = 0
+while (index <=10 ) {
+    //console.log(`value of indexx is ${index}`);
+    index = index+2
+}
+
+
+let myArr =["flash",'supermna','spiderman','hulk']
+let arr =0
+while (arr < myArr.length) {
+   // console.log(`value  is ${myArr[arr]}`);
+    arr ++;
+}
+
+
+let score=11
+do {
+   // console.log(`score is ${score}`);
+    score++
+} while (score <=10);
+
+
+
+let myheros=['salman','saruk','gobinda','bobby','khesari']
+let arrr =0
+do {
+    //console.log(`yes please ${myheros[arrr]}`);
+    arrr++
+} while (arrr >=myheros.length);
